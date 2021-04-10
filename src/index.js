4aa676631f7a3e37ce6798e02e139547190cfe17
Arie Dwi Kusuma <69567276+Arie-Kusuma@users.noreply.github.com>
2021-04-10 13:09:46 +0700

    coba#6-coba hot mudule

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css';
 
 function Ren (){
-  return <h1>coba</h1>;
+  return <h1>jingan</h1>;
 }
 
 ReactDom.render(<Ren/>, document.getElementById('root'));
+if (module.hot) {
+  module.hot.accept();
+}
