154f2d6083202243a17b90d610331877fc5aa059
Arie Dwi Kusuma <69567276+Arie-Kusuma@users.noreply.github.com>
2021-04-10 18:02:22 +0700

    deploy functional app

--- src/index.js
+++ src/index.js
@@ -2,9 +2,35 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-function Ren (){
-  return <h1>asu kabeh</h1>;
+const {useState} = React ;
+
+class Developer{
+
+  constructor(depan,belakang){
+    this.depan = depan;
+    this.belakang = belakang;
+  }
+
+  sikat(){
+    return this.depan+' '+this.belakang;
+  }
 }
+
+const arie = new Developer('arie', 'kusuma');
+
+function Ren (){
+  var [text,fuck] = useState(0);
+  return <>
+
+    <h1>{arie.sikat()}</h1>
+    <h1>{text}</h1>
+    <button onClick ={() => fuck(++text)}>coba</button>
+
+  </>
+
+
+}
+
 
 ReactDom.render(<Ren/>, document.getElementById('root'));
 if (module.hot) {
