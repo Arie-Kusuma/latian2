bb44f2632feae53917fcbc3961a2e7a3a829b106
Arie Dwi Kusuma <69567276+Arie-Kusuma@users.noreply.github.com>
2021-04-10 13:17:58 +0700

    Updates

--- static/js/main.8286746c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonplatian2=this.webpackJsonplatian2||[]).push([[0],{4:function(n,t,e){"use strict";e.r(t);e(0);var c=e(3),i=e.n(c),a=(e(9),e(2));function r(){return Object(a.jsx)("h1",{children:"jingan"})}i.a.render(Object(a.jsx)(r,{}),document.getElementById("root"))},9:function(n,t,e){}},[[4,1,2]]]);
-//# sourceMappingURL=main.8286746c.chunk.js.map
