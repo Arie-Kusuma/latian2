266ec37916cafcf9577b1b8181681da4168ee73f
Arie Dwi Kusuma <69567276+Arie-Kusuma@users.noreply.github.com>
2021-04-23 01:24:43 +0700

    reset-learn hook

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-const {useState} = React ;
+// const {useState} = React ;
 
 class Developer{
 
@@ -18,6 +18,13 @@ class Developer{
 
 const arie = new Developer('arie', 'kusuma');
 
+class Welcome extends React.Component{
+  render(){
+    return<h1>{this.props.name}</h1>;
+  }
+}
+
+
 function Ren (){
   var [text,fuck] = useState(0);
   return <>
@@ -25,11 +32,11 @@ function Ren (){
     <h1>{arie.sikat()}</h1>
     <h1>{text}</h1>
     <button onClick ={() => fuck(++text)}>coba</button>
-
+    <Welcome name="anijng"/>
   </>
 
 
-}
+};
 
 
 ReactDom.render(<Ren/>, document.getElementById('root'));
